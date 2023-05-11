9137cb2aea22ca551df548a160bbf4f81f732359
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-05-11 18:03:51 +0200

    dictionnary done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Footer from './Front/Components/Footer';
 import Header from './Front/Components/Header';
-import Dictionnary from './Front/Components/Dictionnary';
+import Dictionary from './Front/Components/Dictionary';
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <Dictionnary />
+      <Dictionary />
       <Footer />
     </div>
   );
