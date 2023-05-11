20106bfa8e21b54064baa869e2a9e09db8f0ad85
Audrey Alaime <audreyalaime3@gmail.com>
2023-05-11 14:07:10 +0200

    Header-first-version-

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
+import Header from './Front/Components/Header'
 import './App.css'
 
 function App() {
 
   return (
     <div className="app">
-      <h1>
-        Trip planner
-      </h1>
-      <div style={{height: "100%"}}>
-        <div style={{backgroundColor: "var(--main-color)", height: "40%"}}></div>
-        <div style={{backgroundColor: "var(--second-color)", height: "40%"}}></div>
-      </div>
+        <Header/>
     </div>
   )
 }
--- src/Front/Components/Header.jsx
+++ src/Front/Components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import picture from '../assets/Profil.jpg'
+import { useState } from 'react';
 import "./Header.css";
 
 const Header = () => {
+  const [user, setUser] = useState();
+
   return (
-    <div>Header</div>
+    <header>
+          <p>Hi, Ptbambie!</p>
+          <img src={picture} alt='Profil'></img>
+    </header>
   )
 }
 
-export default Header
+export default Header 
