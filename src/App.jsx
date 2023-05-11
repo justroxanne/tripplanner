4832dd1c80c22b7a05866d94a9e37ea117d503cc
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-05-11 11:37:40 +0200

    css init

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 function App() {
 
   return (
-    <><h1>
-      Trip planner
-    </h1>
-    </>
+    <div className="app">
+      <h1>
+        Trip planner
+      </h1>
+      <div style={{height: "100%"}}>
+        <div style={{backgroundColor: "var(--main-color)", height: "40%"}}></div>
+        <div style={{backgroundColor: "var(--second-color)", height: "40%"}}></div>
+      </div>
+    </div>
   )
 }
 
