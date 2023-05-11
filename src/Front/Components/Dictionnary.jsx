8c3a578a188233b3517637ac3e3ebe37b86dcbf0
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-05-11 18:03:51 +0200

    dictionnary done

--- src/Front/Components/Dictionnary.jsx
+++ src/Front/Components/Dictionnary.jsx
@@ -4,32 +4,36 @@ import './Dictionnary.css';
 
 const Dictionnary = () => {
   const [search, setSearch] = useState('');
-  const [research, setResearch] = useState([]);
+  const [research, setResearch] = useState({});
 
   const submit = (e) => {
     e.preventDefault();
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
-      .then((res) => setResearch(res.data));
-    console.log(res.data);
+      .then((res) => setResearch(res.data[0]))
+      .catch((error) => {
+        console.log(error);
+        setResearch({});
+      });
   };
 
   return (
-    <div className='dictionnary-container'>
-      <div className='dictionnary'>
-        <form className='dictionnary-form'>
-          <input
-            className='input'
-            type='text'
-            value={search}
-            onChange={(e) => {
-              setSearch(e.target.value);
-            }}
-            onSubmit={submit}
-            placeholder='What do you want to say ?'
-          />
-        </form>
-        <div className='result'>{research.word}</div>
+    <div className='dictionnary'>
+      <form className='dictionnary-form' onSubmit={submit}>
+        <input
+          className='input'
+          type='text'
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          placeholder='What do you want to say ?'
+        />
+      </form>
+      <div className='result'>
+        {research.meanings
+          ? research.meanings[0].definitions[0].definition
+          : 'No definition found'}
       </div>
     </div>
   );
